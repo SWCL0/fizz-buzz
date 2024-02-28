45694cdb034c671e61500a8add2dd975976707c6
SWCL0 <stan.w.c@icloud.com>
2024-02-28 11:28:29 +0100

    update fizz-buzz.php
    Opdracht gemaakt:
    modulo van 15 - 5 - 3

--- fizz-buzz.php
+++ fizz-buzz.php
@@ -1,5 +1,15 @@
 <?php
 
-
+for ($i = 1; $i <= 50; $i++) {
+    if ($i % 15 == 0) {
+        echo "FIZZ BUZZ" . PHP_EOL;
+    } elseif ($i % 5 == 0) {
+        echo "BUZZ" . PHP_EOL;
+    } elseif ($i % 3 == 0) {
+        echo "FIZZ" . PHP_EOL;
+    } else {
+        echo $i . PHP_EOL;
+    }
+}
 
 ?>
